af8a270a44f78ddca20b8b91608df244c55b0d2e
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-02-08 13:25:34 +0100

    compare variables

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+//Vergleichsoperatoren:
+var num = 5;
+var anotherNum = 8;
+
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != anotherNum);
+console.log(num != 5);
+
